afc2952553b298ba5f5a6440bf4e0c9512eca8ea
Velcorn <iwishyouperish@gmail.com>
2022-02-18 13:16:14 +0100

    Minor text fixes

--- main.py
+++ main.py
@@ -15,15 +15,15 @@ def pdf2xlsx():
     # Get all PDFs in input path
     files = glob(f"{input_path}/*.pdf")
 
-    # Iterate over PDFs, extract tables and load into dataframe
+    # Iterate over PDFs, extract tables and save desired column(s) into dataframe
     print("Extracting tables to dataframe...")
     df = pd.DataFrame()
     for i, f in enumerate(tqdm(files, file=sys.stdout)):
         # Extract table to dataframe
         with pdfplumber.open(f) as pdf:
             table = pdf.pages[0].extract_table()
+            # Temporarily save entire table
             temp = pd.DataFrame(table[1::], columns=table[0])
-            # Add relevant table to df
             # If first PDF, add first and second column, else only second with file name as header
             name = f.split("/")[-1].split("\\")[-1].replace(".pdf", "")
             if i == 0:
@@ -36,6 +36,7 @@ def pdf2xlsx():
     print("Saving dataframe to XLSX...")
     try:
         df.to_excel(file_name, index=False)
+    # Catch error if file is open in another process
     except IOError as e:
         return f"{file_name} is used by another process, please close it first!"
 
